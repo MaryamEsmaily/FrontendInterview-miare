dd68aa3121dccaa35fb544ae205f64a7ada9d813
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2022-11-05 11:09:22 +0330

    add redux-toolkit

---
+++ src/features/transactions/transactionsSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const transactionsSlice = createSlice({
+  name: "transactions",
+  initialState,
+  reducers: {},
+});
+
+export const {} = transactionsSlice.actions;
+
+export default transactionsSlice.reducer;
