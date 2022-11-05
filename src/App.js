e5c2da09a0b4e05890258019816a63b10ea72865
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2022-11-05 10:44:22 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    axios.get("/data.json").then((res) => console.log(res));
+  }, []);
+  return (
+    <div className="App">
+      <div>
+        <p>تمام تراکنش ها</p>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
